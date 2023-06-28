4e224301ae9980d235c975cf7f633c0ac4e1e731
xernomm <rafaelrichie03@gmail.com>
2023-06-28 20:13:35 +0700

    musicfeatures

---
+++ js/music.js
@@ -0,0 +1,43 @@
+var audio = document.getElementById('audioBack');
+var playPauseButton = document.getElementById('playPauseBtn');
+var pause = document.getElementById('pause')
+var play = document.getElementById('play')
+var jazz = document.getElementById('jazz')
+var lofi = document.getElementById('lofi')
+var rock = document.getElementById('rock')
+var pop = document.getElementById('pop')
+var isPlaying = false;
+
+
+playPauseButton.addEventListener('click', function() {
+  if (isPlaying) {
+    audio.pause();
+    
+    pause.classList.add('d-none')
+    pause.classList.remove('d-block')
+    play.classList.add('d-block')
+    play.classList.remove('d-none')
+  } else {
+    audio.play();
+    pause.classList.add('d-block')
+    pause.classList.remove('d-none')
+    play.classList.add('d-none')
+    play.classList.remove('d-block')
+  }
+  isPlaying = !isPlaying;
+});
+
+jazz.addEventListener('click',function(){
+  audio.setAttribute('src','music/jazz.mp3')
+})
+
+lofi.addEventListener('click', function(){
+  audio.setAttribute('src','music/lofi.mp3')
+})
+pop.addEventListener('click', function(){
+  audio.setAttribute('src','music/pop.mp3')
+})
+
+rock.addEventListener('click', function(){
+  audio.setAttribute('src','music/rock.mp3')
+})
