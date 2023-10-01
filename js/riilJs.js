095cfbb341c33a5107cfc7008a91bddcd6d208c6
xernomm <rafaelrichie03@gmail.com>
2023-10-02 00:29:44 +0700

    new projects

--- js/riilJs.js
+++ js/riilJs.js
@@ -20,10 +20,7 @@ let skillCard9 = document.getElementById('skillCard9');
 let skillBody9 = document.getElementById('skillBody9');
 let skillCard10 = document.getElementById('skillCard10');
 let skillBody10 = document.getElementById('skillBody10');
-const nonTech1 = document.getElementById('nonTech1');
-const nonTech2 = document.getElementById('nonTech2');
-const nonTech3 = document.getElementById('nonTech3');
-const nonTech4 = document.getElementById('nonTech4');
+
 let tBody1 = document.getElementById('tBody1')
 let tBody2 = document.getElementById('tBody2')
 let tBody3 = document.getElementById('tBody3')
@@ -116,71 +113,7 @@ skillCard10.addEventListener('mouseenter', function skillCardOne(){
   skillCard10.style.backgroundColor = newBackgroundColor
 });
 
-nonTech1.addEventListener('mouseenter', function skillCardOne(){
-  tBody1.classList.add('d-block')
-  tBody1.classList.remove('d-none')
-  nonTech1.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech1.style.borderRadius=newBorderRadius;
-  nonTech1.style.backgroundColor = newBackgroundColor
-});
 
-nonTech2.addEventListener('mouseenter', function skillCardOne(){
-  tBody2.classList.add('d-block')
-  tBody2.classList.remove('d-none')
-  nonTech2.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech2.style.borderRadius=newBorderRadius;
-  nonTech2.style.backgroundColor = newBackgroundColor
-});
-
-nonTech3.addEventListener('mouseenter', function skillCardOne(){
-  tBody3.classList.add('d-block')
-  tBody3.classList.remove('d-none')
-  nonTech3.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech3.style.borderRadius=newBorderRadius;
-  nonTech3.style.backgroundColor = newBackgroundColor
-});
-
-nonTech4.addEventListener('mouseenter', function skillCardOne(){
- tBody4.classList.add('d-block')
- tBody4.classList.remove('d-none')
- nonTech4.style.transition = "border-radius 0.5s, background-color 0.5s";
- nonTech4.style.borderRadius=newBorderRadius;
- nonTech4.style.backgroundColor = newBackgroundColor
-});
-
-
-// MOUSE LEAVE----------------------------------------------------------------------
-nonTech1.addEventListener('mouseleave', function skillCardLeave1(){
-  tBody1.classList.add('d-none')
-  tBody1.classList.remove('d-block')
-  nonTech1.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech1.style.borderRadius=defaultBorderRadius;
-  nonTech1.style.backgroundColor = defaultBackgroundColor;
-})
-
-nonTech2.addEventListener('mouseleave', function skillCardLeave1(){
-  tBody2.classList.add('d-none')
-  tBody2.classList.remove('d-block')
-  nonTech2.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech2.style.borderRadius=defaultBorderRadius;
-  nonTech2.style.backgroundColor = defaultBackgroundColor;
-})
-
-nonTech3.addEventListener('mouseleave', function skillCardLeave1(){
-  tBody3.classList.add('d-none')
-  tBody3.classList.remove('d-block')
-  nonTech3.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech3.style.borderRadius=defaultBorderRadius;
-  nonTech3.style.backgroundColor = defaultBackgroundColor;
-})
-
-nonTech4.addEventListener('mouseleave', function skillCardLeave1(){
-  tBody4.classList.add('d-none')
-  tBody4.classList.remove('d-block')
-  nonTech4.style.transition = "border-radius 0.5s, background-color 0.5s";
-  nonTech4.style.borderRadius=defaultBorderRadius;
-  nonTech4.style.backgroundColor = defaultBackgroundColor;
-})
 
 
 skillCard1.addEventListener('mouseleave', function skillCardLeave1(){
@@ -302,6 +235,7 @@ const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 }
 // Get the skill cards
 
 const skillsSection = document.getElementById('skillsSection');
+const nonTechSection = document.getElementById('nonTechSection');
 const about1 = document.getElementById('about1')
 const about2 = document.getElementById('about2')
 const about3 = document.getElementById('about3')
@@ -325,16 +259,13 @@ const contacts4= document.getElementById('contacts4')
 
 // Observe each skill card
 observer.observe(skillsSection);
+observer.observe(nonTechSection);
 observer.observe(about1);
 observer.observe(about2);
 observer.observe(about3);
 observer.observe(about4);
 observer.observe(hello);
 observer.observe(iAm);
-observer.observe(nonTech1);
-observer.observe(nonTech2);
-observer.observe(nonTech3);
-observer.observe(nonTech4);
 observer.observe(hobbiesText);
 observer.observe(photosText);
 observer.observe(musicImg);
